b0023f0d8d8af40e48f626fa9468777a22e704fb
cnwobi <c.nwobi@outlook.com>
2018-08-30 06:47:32 +1000

    before adding image view

---
+++ app/src/main/java/hwool/com/au/hermitageintelligenceagency/MainActivity.java
@@ -0,0 +1,23 @@
+package hwool.com.au.hermitageintelligenceagency;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       
+
+    }
+}
