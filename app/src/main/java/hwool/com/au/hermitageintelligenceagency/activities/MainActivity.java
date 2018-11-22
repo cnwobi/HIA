2d217515605ffd53561380f44f28c3d3dfefbba2
innesi <OKyy-p`i2Pb&:Bmp7{P:'ydX+^H/%`>
2018-11-23 01:32:56 +1100

    friday night new design. No functionality.

--- app/src/main/java/hwool/com/au/hermitageintelligenceagency/activities/MainActivity.java
+++ app/src/main/java/hwool/com/au/hermitageintelligenceagency/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package hwool.com.au.hermitageintelligenceagency.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,11 +21,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import hwool.com.au.hermitageintelligenceagency.R;
 import hwool.com.au.hermitageintelligenceagency.models.Quilt;
 
+
+
 public class MainActivity extends AppCompatActivity {
-    //private Button btnScanQr;
-    private Button btnAddNews;
-    private Button btnOptions;
-    public  static TextView tvQrResult;
+    private Button btnScanQR;
+//    private Button btnAddNews;
+//    private Button btnOptions;
+    public static TextView tvQrResult;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +44,24 @@ public class MainActivity extends AppCompatActivity {
         mDatabaseReference.push().setValue(quilt);
 
         //setTitle(R.string.main_title);
-        //btnScanQr  = findViewById(R.id.btScanQr);
-        btnAddNews = findViewById(R.id.btAddNews);
-        tvQrResult = findViewById(R.id.tvQrResult);
-        btnOptions = findViewById(R.id.btnOptions);
+        btnScanQR  = findViewById(R.id.btnScanQR);
+        //btnAddNews = findViewById(R.id.btAddNews);
+        //tvQrResult = findViewById(R.id.tvQrResult);
+        //btnOptions = findViewById(R.id.btnOptions);
+
+
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+        if (actionbar != null) {
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        }
+
+
 
         /*btnScanQr.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,32 +71,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        btnAddNews.setOnClickListener(new View.OnClickListener() {
+        /*btnAddNews.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent =  new Intent(view.getContext(),InsertNews.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
 
-       btnOptions.setOnClickListener(new View.OnClickListener() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        // buuuuuuuuuuug
+        /*btnScanQR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), OptionActivity.class);
+                Intent intent = new Intent(view.getContext(), ScanActivity.class);
                 startActivity(intent);
             }
-        });
-
-
-
-
+        });*/
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     @Override
     public void setTitle(int titleId) {
         super.setTitle(titleId);
     }
+
 
 }
 
