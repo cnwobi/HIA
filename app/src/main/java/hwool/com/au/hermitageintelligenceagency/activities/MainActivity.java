78f9c09b86e0a326e19a731bc2b1b974b9788f66
innesi <OKyy-p`i2Pb&:Bmp7{P:'ydX+^H/%`>
2019-04-07 13:02:17 +1000

    added the overflow menu

--- app/src/main/java/hwool/com/au/hermitageintelligenceagency/activities/MainActivity.java
+++ app/src/main/java/hwool/com/au/hermitageintelligenceagency/activities/MainActivity.java
@@ -1,8 +1,16 @@
 package hwool.com.au.hermitageintelligenceagency.activities;
 
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.google.firebase.database.DatabaseReference;
@@ -10,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import hwool.com.au.hermitageintelligenceagency.R;
 import hwool.com.au.hermitageintelligenceagency.models.Quilt;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -38,21 +47,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-        /*Toolbar toolbar = findViewById(R.id.top_toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        ActionBar actionbar = getSupportActionBar();
+        /*ActionBar actionbar = getSupportActionBar();
         if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true);
         }
         if (actionbar != null) {
             actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-        }
+        }*/
 
 
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
+        /*mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
 
@@ -123,6 +131,37 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_scanQR:
+                //Log.i("test", "test_scan");
+                Intent intentScanQR = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intentScanQR);
+                return true;
+            case R.id.nav_connect_quilt:
+                Toast.makeText(getApplicationContext(), "Smart Quilt not available", Toast.LENGTH_LONG).show();
+                //Log.i("test_toast", "test_toast");
+                return true;
+            case R.id.nav_customise:
+                Intent intentCustomise = new Intent(MainActivity.this, CustomiseQuiltActivity.class);
+                startActivity(intentCustomise);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
 }
 
