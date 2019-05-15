f72d05b074ce4d7a1d80b5f288a724996a5cb712
innesi <OKyy-p`i2Pb&:Bmp7{P:'ydX+^H/%`>
2019-05-15 17:38:09 +1000

    customise quilt change spacing change background color

--- app/src/main/java/au/com/hermitagewool/services/MyFirebaseMessagingService.java
+++ app/src/main/java/au/com/hermitagewool/services/MyFirebaseMessagingService.java
@@ -13,28 +13,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if(remoteMessage.getData().size()>0){
+        if(remoteMessage.getData().size() > 0) {
             Log.d(TAG, "onMessageReceived: "+remoteMessage.getData());
             JSONObject data =  new JSONObject(remoteMessage.getData());
 
             try {
                 String jsonMessage= data.getString("extra_information");
-                Log.d(TAG, "onMessageReceived: \n"+ "Extra Information "+ jsonMessage);
+                Log.d(TAG, "onMessageReceived: \n" + "Extra Information "+ jsonMessage);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        if(remoteMessage.getNotification()!=null){
-            String title = remoteMessage.getNotification().getTitle();
-            String message = remoteMessage.getNotification().getBody();
-            String clickAction= remoteMessage.getNotification().getClickAction();
+        if(remoteMessage.getNotification() != null) {
+            String title       = remoteMessage.getNotification().getTitle();
+            String message     = remoteMessage.getNotification().getBody();
+            String clickAction = remoteMessage.getNotification().getClickAction();
 
 
             Log.d(TAG, "Message Title: " + title);
-            Log.d(TAG, "Mmessage Notification Body:"+ message);
+            Log.d(TAG, "Message Notification Body:"+ message);
             Log.d(TAG, "Message Notification click_action: "+ clickAction);
-
         }
     }
 }
