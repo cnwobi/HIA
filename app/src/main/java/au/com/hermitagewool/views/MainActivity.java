11f65752ca744fb9c55d874673dadc84ff28babb
cnwobi <c.nwobi@outlook.com>
2019-05-22 21:30:58 +1000

    Ipsum generator

--- app/src/main/java/au/com/hermitagewool/views/MainActivity.java
+++ app/src/main/java/au/com/hermitagewool/views/MainActivity.java
@@ -54,32 +54,21 @@ public class MainActivity extends AppCompatActivity {
         FirebaseMessaging.getInstance().subscribeToTopic("NewsLetter");
 
 
-        /* * / // remove the space before the last / to uncomment
+
         Date c = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
         String formattedDate = df.format(c);
 
-        News news1 = new News();
-        news1.setmTitle("Test5");
-        news1.setmBody("Test\n" +
-                "Test\n" +
-                "Test\n" +
-                "Test\n" +
-                "Test\n" +
-                "Test\n" +
-                "Test\n" +
-                "\n\n\n" +
-                "Test\n" +
-                "Test\n" +
-                "\n\n\n" +
-                "Test\n" +
-                "Test\n");
+        /*News news1 = new News();
+        news1.setmTitle("Test More than 40 CAPS");
+        news1.setmBody("LOREM IPSUM GENERATOR\n" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
         news1.setmAuthor("The Joker");
         news1.setmCreationDate(formattedDate);
         NewsRepositiory newsRepositiory = new NewsRepositoryImpl();
 
         newsRepositiory.saveNews(news1);
-        /* */
+*/
     }
 
     @Override
@@ -91,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.nav_scanQR:
                 Intent intentScanQR = new Intent(MainActivity.this, ScanActivity.class);
